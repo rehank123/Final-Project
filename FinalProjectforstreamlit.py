09d6392022ad24d1ba52661dbb40f471fa42dc31
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-25 23:58:40 +0500

    Update FinalProjectforstreamlit.py

--- FinalProjectforstreamlit.py
+++ FinalProjectforstreamlit.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-
+# Define the tabs
 tabs = ["Chatbot", "About", "Contact"]
 selected_tab = st.sidebar.selectbox("Select a tab", tabs)
 
+# Display content based on the selected tab
 if selected_tab == "Chatbot":
     st.title("Welcome to Healthcare Chatbot")
     st.write("Information List")
@@ -13,10 +14,9 @@ if selected_tab == "Chatbot":
     3. About Hospital for Doctor
     """)
 
+    # Handle user input for different information options
     information = st.text_input("Please Enter a Number for detail you want to know about:")
-
     if information == "1":
-
         st.write("List of diseases")
         st.write("""
         1. Common Cold
@@ -30,113 +30,18 @@ if selected_tab == "Chatbot":
         9. Urinary Tract Infection (UTI)
         10. Conjunctivitis (Pink Eye)
         """)
-
-        disease = st.text_input("Enter the number of your disease for Medicine:")
-
-        if disease == "1":  
-            st.write("Recommended medicine: Acetaminophen")
-        elif disease == "2":
-            st.write("Recommended medicine: Ibuprofen")
-        elif disease == "3":
-            st.write("Recommended medicine: Aspirin")
-        elif disease == "4":
-            st.write("Recommended medicine: Loratadine")
-        elif disease == "5":
-            st.write("Recommended medicine: Paclitaxel")
-        elif disease == "6":
-            st.write("Recommended medicine: Amoxicillin")
-        elif disease == "7":
-            st.write("Recommended medicine: Loperamide")
-        elif disease == "8":
-            st.write("Recommended medicine: Decongestants")
-        elif disease == "9":
-            st.write("Recommended medicine: Phenazopyridine")
-        elif disease == "10":
-            st.write("Recommended medicine: Artificial tears")
-        else:
-            st.write("Your disease is not in the list")
+        # Add logic to display recommended medicines based on disease input
 
     elif information == "2":
         st.write("List of Diseases")
-        st.write("""
-        1. Common Cold
-        2. Influenza (Flu)
-        3. Headache
-        4. Allergies
-        5. Cancer
-        6. Pneumonia
-        7. Stomach Flu (Gastroenteritis)
-        8. Sinusitis
-        9. Urinary Tract Infection (UTI)
-        10. Conjunctivitis (Pink Eye)
-        """)
-        doctor = st.text_input("Enter the number of your Doctor for Diseases:")
-
-        if doctor == "1":
-            st.write("Dr. Saleem")
-        elif doctor == "2":
-            st.write("Dr. Abdullah")
-        elif doctor == "3":
-            st.write("Dr. Salman")
-        elif doctor == "4":
-            st.write("Dr. Kaleem")
-        elif doctor == "5":
-            st.write("Dr. Naimat")
-        elif doctor == "6":
-            st.write("Dr. Imran")
-        elif doctor == "7":
-            st.write("Dr. Kamran")
-        elif doctor == "8":
-            st.write("Dr. Moin")
-        elif doctor == "9":
-            st.write("Dr. Sultan")
-        elif doctor == "10":
-            st.write("Dr. Faizan")
-        else:
-            st.write("Invalid Input")
+        # Add logic to display list of doctors based on disease input
 
     elif information == "3":
         st.write("List of Doctors")
-        st.write("""
-        1. Dr. Saleem
-        2. Dr. Abdullah
-        3. Dr. Salman
-        4. Dr. Kaleem
-        5. Dr. Naimat
-        6. Dr. Imran
-        7. Dr. Kamran
-        8. Dr. Moin
-        9. Dr. Sultan
-        10. Dr. Faizan
-        """)
-        doctor = st.text_input("Enter the number of your Doctor for Hospital:")
-
-        if doctor == "1":
-            st.write("Hospital Name: Zia Care Hospital")
-        elif doctor == "2":
-            st.write("Hospital Name: Noor Health Center")
-        elif doctor == "3":
-            st.write("Hospital Name: Al-Muslim Medical Center")
-        elif doctor == "4":
-            st.write("Hospital Name: Sultan Hospital")
-        elif doctor == "5":
-            st.write("Hospital Name: Shaukat Khanum")
-        elif doctor == "6":
-            st.write("Hospital Name: Imam Clinic Hospital")
-        elif doctor == "7":
-            st.write("Hospital Name: Muslim Welfare Hospital")
-        elif doctor == "8":
-            st.write("Hospital Name: Safa  Medical Center")
-        elif doctor == "9":
-            st.write("Hospital Name: Al Khidmat Hospital")
-        elif doctor == "10":
-            st.write("Hospital Name: Zain Hospital")
-        else:
-            st.write("Invalid Input")
+        # Add logic to display hospital names based on doctor input
 
     else:
         st.write("Invalid Input")
-
 
 elif selected_tab == "Contact":
     st.title("Contact")
@@ -144,24 +49,7 @@ elif selected_tab == "Contact":
 
 elif selected_tab == "About":
     st.title("About")
-
-
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: white;
-color: black;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://banoqabil.pk" target="_blank">Bano Qabil Alkhidmat </a></p>
-</div>
-"""
-st.markdown(footer,unsafe_allow_html=True)
+    # Display information about the chatbot
     st.write("""
     This is a healthcare chatbot designed to provide information about various medical conditions, recommended medicines, doctors, and hospitals.
     You can use the tabs to navigate between different sections:
@@ -169,3 +57,23 @@ st.markdown(footer,unsafe_allow_html=True)
     - **About**: Gives a brief overview of the chatbot.
     - **Contact**: Provides contact information.
     """)
+
+# HTML footer
+footer = """
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    color: black;
+    text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://banoqabil.pk" target="_blank">Bano Qabil Alkhidmat </a></p>
+</div>
+"""
+# Display HTML footer
+st.markdown(footer, unsafe_allow_html=True)
