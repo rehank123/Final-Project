72500d3cfaad7bc5f7eaa7cf523c4affa6d30189
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-25 23:34:00 +0500

    Update FinalProjectforstreamlit.py

--- FinalProjectforstreamlit.py
+++ FinalProjectforstreamlit.py
@@ -1,16 +1,167 @@
 import streamlit as st
 
-# Create tabs
-tabs = ["Tab 1", "Tab 2", "Tab 3"]
-selected_tab = st.sidebar.selectbox("Select a tab", tabs)
+def main():
+    st.title("Welcome to My Streamlit App")
+    st.write("""
+    This is the home page of my Streamlit app. 
+    Use the sidebar to navigate to different sections.
+    """)
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ["Home", "About", "Contact"])
 
-# Display content based on selected tab
-if selected_tab == "Tab 1":
-    st.write("This is content for Tab 1")
-elif selected_tab == "Tab 2":
-    st.write("This is content for Tab 2")
-elif selected_tab == "Tab 3":
-    st.write("This is content for Tab 3")
+    if page == "Chatbot":
+        show_home_page()
+st.title("Welcome to Healthcare Chatbot")
+st.write("Information List")
+st.write("""
+1. About Medicine for Disease
+2. About Doctor for Disease
+3. About Hospital for Doctor
+""")
+
+information = st.text_input("Please Enter a Number for detail you want to know about:")
+
+if information == "1":
+
+  st.write("List of diseases")
+  st.write("""
+  1. Common Cold
+  2. Influenza (Flu)
+  3. Headache
+  4. Allergies
+  5. Cancer
+  6. Pneumonia
+  7. Stomach Flu (Gastroenteritis)
+  8. Sinusitis
+  9. Urinary Tract Infection (UTI)
+  10. Conjunctivitis (Pink Eye)
+  """)
+
+  disease = st.text_input("Enter the number of your disease for Medicine:")
+
+  if disease == "1":  
+    st.write("Recommended medicine: Acetaminophen")
+  elif disease == "2":
+    st.write("Recommended medicine: Ibuprofen")
+  elif disease == "3":
+    st.write("Recommended medicine: Aspirin")
+  elif disease == "4":
+    st.write("Recommended medicine: Loratadine")
+  elif disease == "5":
+    st.write("Recommended medicine: Paclitaxel")
+  elif disease == "6":
+    st.write("Recommended medicine: Amoxicillin")
+  elif disease == "7":
+    st.write("Recommended medicine: Loperamide")
+  elif disease == "8":
+    st.write("Recommended medicine: Decongestants")
+  elif disease == "9":
+    st.write("Recommended medicine: Phenazopyridine")
+  elif disease == "10":
+    st.write("Recommended medicine: Artificial tears")
+  else:
+    st.write("Your disease is not in the list")
+
+elif information == "2":
+  st.write("List of Diseases")
+  st.write("""
+   1. Common Cold
+   2. Influenza (Flu)
+   3. Headache
+   4. Allergies
+   5. Cancer
+   6. Pneumonia
+   7. Stomach Flu (Gastroenteritis)
+   8. Sinusitis
+   9. Urinary Tract Infection (UTI)
+   10. Conjunctivitis (Pink Eye)
+   """)
+  doctor = st.text_input("Enter the number of your Doctor for Diseases:")
+
+  if doctor == "1":
+    st.write("Dr. Saleem")
+  elif doctor == "2":
+    st.write("Dr. Abdullah")
+  elif doctor == "3":
+    st.write("Dr. Salman")
+  elif doctor == "4":
+    st.write("Dr. Kaleem")
+  elif doctor == "5":
+    st.write("Dr. Naimat")
+  elif doctor == "6":
+    st.write("Dr. Imran")
+  elif doctor == "7":
+    st.write("Dr. Kamran")
+  elif doctor == "8":
+    st.write("Dr. Moin")
+  elif doctor == "9":
+    st.write("Dr. Sultan")
+  elif doctor == "10":
+    st.write("Dr. Faizan")
+  else:
+    st.write("Invalid Input")
+
+elif information == "3":
+  st.write("List of Doctors")
+  st.write("""
+  1. Dr. Saleem
+  2. Dr. Abdullah
+  3. Dr. Salman
+  4. Dr. Kaleem
+  5. Dr. Naimat
+  6. Dr. Imran
+  7. Dr. Kamran
+  8. Dr. Moin
+  9. Dr. Sultan
+  10. Dr. Faizan
+  """)
+  doctor = st.text_input("Enter the number of your Doctor for Hospital:")
+
+  if doctor == "1":
+    st.write("Hospital Name: Zia Care Hospital")
+  elif doctor == "2":
+    st.write("Hospital Name: Noor Health Center")
+  elif doctor == "3":
+    st.write("Hospital Name: Al-Muslim Medical Center")
+  elif doctor == "4":
+    st.write("Hospital Name: Sultan Hospital")
+  elif doctor == "5":
+    st.write("Hospital Name: Shaukat Khanum")
+  elif doctor == "6":
+    st.write("Hospital Name: Imam Clinic Hospital")
+  elif doctor == "7":
+    st.write("Hospital Name: Muslim Welfare Hospital")
+  elif doctor == "8":
+    st.write("Hospital Name: Safa  Medical Center")
+  elif doctor == "9":
+    st.write("Hospital Name: Al Khidmat Hospital")
+  elif doctor == "10":
+    st.write("Hospital Name: Zain Hospital")
+
+
+
+
+
+
+
+    
+    elif page == "About":
+        show_about_page()
+    elif page == "Contact":
+        show_contact_page()
+
+def show_home_page():
+    st.write("This is the home page. You can add more content here.")
+
+def show_about_page():
+    st.write("This is the about page. You can provide information about your app or yourself here.")
+
+def show_contact_page():
+    st.write("This is the contact page. You can provide contact information or a form for users to reach you.")
+
+if __name__ == "__main__":
+    main()
+
 
 
 
