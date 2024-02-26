fe6b11d4c22e8d4456e9083f80c7aa57f197073a
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-26 16:27:21 +0500

    Update FinalProjectforstreamlit.py

--- FinalProjectforstreamlit.py
+++ FinalProjectforstreamlit.py
@@ -254,18 +254,7 @@ elif selected_tab == "Appointment Data":
 
 
 
-def hospital_addresses_page():
-    st.title("Hospital Addresses")
-    st.write("Content for the Hospital Addresses tab goes here.")
 
-    # URL of the hospital building image
-    image_url = "https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=612x612&w=0&k=20&c=oUILskmtaPiA711DP53DFhOUvE7pfdNeEK9CfyxlGio%3D"
-
-    # Display the hospital building image
-    st.image(image_url, caption="Hospital Building", use_column_width=True)
-
-if __name__ == "__main__":
-    hospital_addresses_page()
 
 elif selected_tab == "Upload Tests":
     st.title("Upload Tests")
